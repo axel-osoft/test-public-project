38c931f077ada2297fc43a89274b82fd2fcc0deb
Alexandre Odoux <alexandre.odoux@sonarsource.com>
2024-04-08 16:28:17 +0200

    Commit nb1

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,7 +3,6 @@ package hello;
 public class HelloWorld {
   public static void main(String[] args) {
 	Greeter greeter = new Greeter();
-	@SuppressWarnings("java:S1481")
 	var hello = "Hello";
 	System.out.println(greeter.sayHello());
   }
